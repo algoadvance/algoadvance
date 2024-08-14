1793b7841672d7e9ed19d85542c0fa87e9888256
James <jamesdcheng@gmail.com>
2024-08-14 13:56:26 -0400

    try it out with 2 files

---
+++ copy_and_add_jekyll.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import sys
+
+def copy_and_add_jekyll(source_dir, destination_dir):
+    for filename in os.listdir(source_dir):
+        if filename.endswith('.html'):
+           if os.path.isfile(os.path.join(source_dir, filename)):
+            name, extension = os.path.splitext(filename)
+            character = name.split('.')[0] 
+            title = name.split('.')[1]
+            with open(os.path.join(source_dir, filename), 'r') as file:
+                content = file.read()
+            link = '''
+---
+layout: page
+title: %s
+permalink: /l%s
+---
+            '''%(title, character)
+            new_content = link + content
+            with open(os.path.join(destination_dir, filename), 'w') as file:
+                file.write(new_content)
+            # new_filename = character + filename
+            # source_path = os.path.join(source_dir, filename)
+            # destination_path = os.path.join(destination_dir, new_filename)
+            # shutil.copy2(source_path, destination_path)
+        
+
+if __name__ == "__main__":
+    # if len(sys.argv) < 3:
+    #     print("Usage: python copy_and_add_jekyll.py <source_dir> <destination_dir>")
+    #     sys.exit(1)
+
+    # source_dir = sys.argv[1]
+    # destination_dir = sys.argv[2]
+    copy_and_add_jekyll("/home/darulebreaker/repos/Leetcode-Questions-Scraper/", "/home/darulebreaker/repos/algoadvance/problems/")
